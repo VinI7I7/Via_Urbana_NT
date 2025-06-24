be4d3b2bfcded0ccd54bde5aec02dc7c1d9ae44b
thiago-catolica <thiago.castro@a.ucb.br>
2025-06-23 23:46:21 -0300

    Delete Downloads/Via_Urbana_NT-main/Via_Urbana_NT-main/ViaUrbana_Project directory

--- Downloads/Via_Urbana_NT-main/Via_Urbana_NT-main/ViaUrbana_Project/relatos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Relato
-
-class RelatoForm(forms.ModelForm):
-    class Meta:
-        model = Relato
-        fields = ['titulo', 'tipo', 'categoria', 'descricao', 'localizacao', 'foto']
--- Downloads/Via_Urbana_NT-main/Via_Urbana_NT-main/ViaUrbana_Project/usuarios/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.forms import UserCreationForm
-from .models import UsuarioCustomizado
-
-class CadastroUsuarioForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = UsuarioCustomizado
-        fields = UserCreationForm.Meta.fields + ('email',) 
-                                                          
